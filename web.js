54e206cb81cd4fae6740fb0e55bd20738a8826e3
Rafael Tikva <rafitikva@hotmail.com>
2013-07-06 23:30:27 +0000

    read from index.html, master

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+
+var home = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFileSync(home));
 });
 
 var port = process.env.PORT || 5000;
