8577d4a318dfe390ac9c7790be67895fb4148b5a
Rafael Tikva <rafitikva@hotmail.com>
2013-07-11 04:27:53 +0000

    working grader.js

---
+++ graderworks.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
+console.log (process.argv);
+var Result = null;
+var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var rest = require ('restler');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+var getRestResult = function(URL) {
+    console.log("entered restGetResult");
+    console.log("before GET, the url is " + URL);
+    URL = URL.toString();
+    rest.get("http://"+URL).on('complete', function (result,response) {
+        console.log("Entered GET oncomplete function\n");
+        if (result instanceof Error) {
+            console.log("%s does not exist.", URL);
+            process.exit(1);
+            }
+        else {
+	     fs.writeFileSync(URL,result);
+             var checkJson = checkHtmlFile(program.url, program.checks);console.log("printed in case result came back correct \n");
+             var outJson = JSON.stringify(checkJson, null, 4);
+             console.log(outJson);
+	     fs.unlinkSync(URL);
+	}});
+return URL;
+};
+
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL>', 'Website URL', clone(getRestResult))
+        .parse(process.argv);
+
+process.argv.forEach(function(input){	
+    if (input === '-f' || input === '--file') {
+	console.log("entered --file: " + program.file);
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+	}
+    else {
+    exports.checkHtmlFile = checkHtmlFile;
+    }
+})};
+
+
